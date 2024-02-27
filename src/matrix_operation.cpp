26425fa56de5f276c67edbf0868eca178621c94c
Thien <nguyenhthien2001@gmail.com>
2024-02-27 11:27:43 -0600

    simpled multiply

--- src/matrix_operation.cpp
+++ src/matrix_operation.cpp
@@ -6,6 +6,5 @@ MatrixOperations::MatrixOperations(/* args */) {
 }
 
 Eigen::MatrixXd MatrixOperations::multiply(const Eigen::MatrixXd &mat1, const Eigen::MatrixXd &mat2) {
-    Eigen::MatrixXd mul = mat1*mat2;
-    return mul;
+    return mat1*mat2;
 }
